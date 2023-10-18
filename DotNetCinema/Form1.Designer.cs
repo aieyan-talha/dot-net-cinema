8fb45c9ad12fda465366293bbd122e2911fb3fec
aieyan-helium <133630723+aieyan-helium@users.noreply.github.com>
2023-10-18 16:40:09 +1100

    Successful DB Setup

--- DotNetCinema/Form1.Designer.cs
+++ DotNetCinema/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             header = new Label();
+            getData = new Button();
+            dummyData = new ListBox();
             SuspendLayout();
             // 
             // header
@@ -42,11 +44,32 @@
             header.Text = "Dot Net Cinema";
             header.Click += header_Click;
             // 
+            // getData
+            // 
+            getData.Location = new Point(276, 219);
+            getData.Name = "getData";
+            getData.Size = new Size(216, 29);
+            getData.TabIndex = 1;
+            getData.Text = "Get Data";
+            getData.UseVisualStyleBackColor = true;
+            getData.Click += getData_Click;
+            // 
+            // dummyData
+            // 
+            dummyData.FormattingEnabled = true;
+            dummyData.ItemHeight = 20;
+            dummyData.Location = new Point(310, 254);
+            dummyData.Name = "dummyData";
+            dummyData.Size = new Size(150, 104);
+            dummyData.TabIndex = 2;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(dummyData);
+            Controls.Add(getData);
             Controls.Add(header);
             Name = "Form1";
             Text = "Dot Net Cinema";
@@ -57,5 +80,7 @@
         #endregion
 
         private Label header;
+        private Button getData;
+        private ListBox dummyData;
     }
 }
--- DotNetCinema/Form1.cs
+++ DotNetCinema/Form1.cs
@@ -1,7 +1,12 @@
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace DotNetCinema
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Projects\\UTS\\Dot Net\\dot-net-cinema\\DotNetCinema\\ApplicationDatabase.mdf\";Integrated Security=True";
+
         public Form1()
         {
             InitializeComponent();
@@ -10,6 +15,28 @@ namespace DotNetCinema
         private void header_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void getData_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection SqlConnection = new SqlConnection(@connectionString))
+            {;
+                SqlConnection.Open();
+                string query = "SELECT * FROM Users";
+
+                using (SqlCommand command = new SqlCommand(query, SqlConnection))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var data = reader["first_name"].ToString();
+                        // Assuming you have a ListBox named listBox1 on your form
+                        dummyData.Items.Add(data ?? "");
+                    }
+                }
+            }
+
+            
         }
     }
 }
