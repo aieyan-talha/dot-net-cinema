2e370f1a8880fa8c0a6788bbbe89ab4442dd8c2f
hyun.heather.park <joorohi@gmail.com>
2023-10-28 14:56:14 +1100

    Created User Pages

---
+++ DotNetCinema/userPage.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace DotNetCinema
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Bauhaus 93", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(12, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 23);
+            label1.TabIndex = 0;
+            label1.Text = "DOT NET CINEMA";
+            label1.Click += label1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            comboBox1.BackColor = Color.Black;
+            comboBox1.FlatStyle = FlatStyle.Popup;
+            comboBox1.Font = new Font("Calibri", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            comboBox1.ForeColor = SystemColors.ButtonFace;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "My Tickets", "My Watchlist", "My Points", "Sign Out" });
+            comboBox1.Location = new Point(1179, 17);
+            comboBox1.Name = "comboBox1";
+            comboBox1.RightToLeft = RightToLeft.No;
+            comboBox1.Size = new Size(155, 30);
+            comboBox1.TabIndex = 1;
+            comboBox1.Text = "MY PAGE";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.BackColor = SystemColors.ButtonFace;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.ForeColor = SystemColors.ButtonFace;
+            textBox1.Location = new Point(203, 58);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Search";
+            textBox1.Size = new Size(948, 24);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 6F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(536, 58);
+            button1.Name = "button1";
+            button1.Size = new Size(67, 24);
+            button1.TabIndex = 3;
+            button1.Text = "FIND";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Bauhaus 93", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkRed;
+            label2.Location = new Point(280, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(236, 36);
+            label2.TabIndex = 4;
+            label2.Text = "NOW SHOWING";
+            label2.Click += label2_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(112, 174);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(145, 173);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox2.Location = new Point(326, 174);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(693, 173);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            pictureBox3.Location = new Point(536, 174);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(693, 173);
+            pictureBox3.TabIndex = 7;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(157, 361);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 22);
+            label3.TabIndex = 8;
+            label3.Text = "NAME";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Calibri", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(372, 361);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 22);
+            label4.TabIndex = 9;
+            label4.Text = "NAME";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(1128, 361);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 22);
+            label5.TabIndex = 10;
+            label5.Text = "NAME";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(151, 383);
+            label6.Name = "label6";
+            label6.Size = new Size(68, 21);
+            label6.TabIndex = 11;
+            label6.Text = "166 min";
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(365, 383);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 21);
+            label7.TabIndex = 12;
+            label7.Text = "166 min";
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(1122, 383);
+            label8.Name = "label8";
+            label8.Size = new Size(68, 21);
+            label8.TabIndex = 13;
+            label8.Text = "166 min";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkRed;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(141, 416);
+            button2.Name = "button2";
+            button2.Size = new Size(88, 31);
+            button2.TabIndex = 14;
+            button2.Text = "BOOK";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button3.BackColor = Color.DarkRed;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(357, 416);
+            button3.Name = "button3";
+            button3.Size = new Size(636, 31);
+            button3.TabIndex = 15;
+            button3.Text = "BOOK";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button4.BackColor = Color.DarkRed;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Calibri", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(1115, 416);
+            button4.Name = "button4";
+            button4.Size = new Size(88, 31);
+            button4.TabIndex = 16;
+            button4.Text = "BOOK";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(44, 44, 44);
+            ClientSize = new Size(1360, 869);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ButtonFace;
+            MaximumSize = new Size(1382, 925);
+            Name = "Form2";
+            Text = "Dot Net Cinema";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
